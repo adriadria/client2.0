556b2a34ba2c76b85375845caa7ac4a3b8272fa2
adriadria <orochimei11@gmail.com>
2021-11-08 02:37:57 -0300

    fix v 2.0 ci

--- src/ApiReq/constantInfo.js
+++ src/ApiReq/constantInfo.js
@@ -4,23 +4,23 @@ import {BASIC_URL} from '../constants'
 export function getCategories(feat){
     return axios.get(`${BASIC_URL}/categories${feat?'?feat=true':''}`)
     .then(r=>r.data)
-    .catch(r=>r.response.data)
+    .catch(e => e)
 }
 
 export function getTips(){
     return axios.get(`${BASIC_URL}/tips`)
     .then(r=>r.data)
-    .catch(r=>r.response.data)
+    .catch(e => e)
 }
 
 export function getReviews(userId){
     return axios.get(`${BASIC_URL}/reviews${userId ? '?userId='+userId : ''}`)
     .then(r=>r.data)
-    .catch(r=>r.response.data)
+    .catch(e => e)
 }
 
 export function getCountries(){
     return axios.get(`${BASIC_URL}/countries`)
     .then(r=>r.data)
-    .catch(r=>r.response.data)
+    .catch(e => e)
 }
